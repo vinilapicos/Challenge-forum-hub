178facbcc861bb4108b74f79e154a2b415892b28
Vinicius Carvalho <vinilapicos@gmail.com>
2026-03-10 11:03:57 -0300

    Inicio da aplicação: commit inicial com esqueleto do Spring Boot

---
+++ src/main/java/io/github/vinilapicos/challenge_forum_hub/ChallengeForumHubApplication.java
@@ -0,0 +1,13 @@
+package io.github.vinilapicos.challenge_forum_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeForumHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeForumHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/vinilapicos/challenge_forum_hub/ChallengeForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vinilapicos.challenge_forum_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
